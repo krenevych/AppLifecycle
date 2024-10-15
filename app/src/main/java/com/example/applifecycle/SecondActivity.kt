2f18927ca1f6d69bde16ba00726fd94f9fd0117a
Andrii Krenevych <krenevych@knu.ua>
2024-10-15 09:28:39 +0300

    added logs for lifecycle

--- app/src/main/java/com/example/applifecycle/SecondActivity.kt
+++ app/src/main/java/com/example/applifecycle/SecondActivity.kt
@@ -2,6 +2,7 @@ package com.example.applifecycle
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.applifecycle.databinding.ActivitySecondBinding
 
@@ -18,6 +19,41 @@ class SecondActivity : AppCompatActivity() {
             val intent = Intent(this, TransparantActivity::class.java)
             startActivity(intent)
         }
+
+        Log.d(TAG, "onCreate: ${this::class.simpleName}")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ${this::class.simpleName}")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "onResume: ${this::class.simpleName}")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, "onPause: ${this::class.simpleName}")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "onStop: ${this::class.simpleName}")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy: ${this::class.simpleName}")
 
     }
 }
