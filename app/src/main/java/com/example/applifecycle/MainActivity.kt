e15af5d67ce3deb87b3b0e82e672d0afccc03036
Andrii Krenevych <krenevych@knu.ua>
2024-10-20 23:16:00 +0300

    ProcessLifecycleOwner

--- app/src/main/java/com/example/applifecycle/MainActivity.kt
+++ app/src/main/java/com/example/applifecycle/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.applifecycle
 
-import android.app.Application
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.applifecycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
